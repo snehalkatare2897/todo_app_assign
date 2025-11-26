82f45f7eff3ede05a080008a1834098e2e5add6c
MAYURI KATARE <kataresnehal28@gmail.com>
2025-11-26 11:12:47 +0530

    update the source code

--- src/middlewares/middlewareRole.js
+++
@@ -1,9 +0,0 @@
-// const { userAuth } = require("../middlewares/middlewareAuth");
-// const roleAuth= (userAuth,async (req, res, next) => {
-//     (...roles) => {
-//     if (!roles.includes(req.user.role))
-//       return res.status(403).json({ message: "Access Denied" });
-//     next();
-//   };
-// });
-// module.exports = {roleAuth}
